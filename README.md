bb3b1b87931c86ae621fa095f9468c6eacec5cbe
Feer Aleksandr <feer@retailsuite.io>
2018-08-11 19:37:23 +0300

    add deleteFolder

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 ### Доступные команды
 
 * `yarn run codestyle` - проверить стиль кода
-* `node sort.js ./music ./sort deleted` - сортировка файлов с удаением
+* `node sort.js ./music ./sort deleted` - сортировка файлов с удалением
 * `node sort.js ./music ./sort` - сортировка файлов без удаления
--- sort.js
+++ sort.js
@@ -1,44 +1,59 @@
 let fs = require('fs');
 let path = require('path');
-let base = process.argv.slice(2)[0];
-let final = process.argv.slice(2)[1];
+let baseFolder = process.argv.slice(2)[0];
+let finalFolder = process.argv.slice(2)[1];
 let deleted = process.argv.slice(2)[2] === 'deleted';
 
-const readDir = (base) => {
-  const files = fs.readdirSync(base);
+const deleteDir = (folder) => {
+  const files = fs.readdirSync(folder);
 
   files.forEach(file => {
-    let localBase = path.join(base, file);
-    let state = fs.statSync(localBase);
+    let fileName = path.join(folder, file);
+    let state = fs.statSync(fileName);
 
     if (state.isDirectory()) {
-      readDir(localBase);
+      deleteDir(fileName);
     } else {
-      let localFinal = path.join(final, file[0]);
+      fs.unlinkSync(fileName, (err) => {
+        if (err) throw err;
+      });
+    }
+  });
 
-      fs.readFile(localBase, (err, buffer) => {
+  fs.rmdirSync(folder, (err) => {
+    if (err) throw err;
+  });
+}
+
+const readDir = (folder) => {
+  const files = fs.readdirSync(folder);
+
+  files.forEach(file => {
+    let fileName = path.join(folder, file);
+    let state = fs.statSync(fileName);
+
+    if (state.isDirectory()) {
+      readDir(fileName);
+    } else {
+      let newFolder = path.join(finalFolder, file[0]);
+
+      if (!fs.existsSync(newFolder)) {
+        fs.mkdirSync(newFolder);
+      }
+
+      fs.readFileSync(fileName, (err, buffer) => {
         if (err) throw err;
 
-        if (!fs.existsSync(localFinal)) {
-          fs.mkdirSync(localFinal);
-        }
-
-        fs.writeFile(path.join(localFinal, file), buffer, err => {
+        fs.writeFileSync(path.join(newFolder, file), buffer, err => {
           if (err) throw err;
-          
-          if (deleted) {
-            fs.unlink(path.join(localBase), (err) => {
-              if (err) throw err;
-            });
-          }
         });
       });
     }
   });
 };
 
-if (!fs.existsSync(final)) {
-  fs.mkdirSync(final);
-}
+readDir(baseFolder);
 
-readDir(base);
+if (deleted) {
+  deleteDir(baseFolder);
+}
