9808bfd3c932dc47f449771cd5f8f1b527585de6
Feer Aleksandr <feer@retailsuite.io>
2018-08-13 21:13:33 +0300

    async copy files

--- sort.js
+++ sort.js
@@ -1,20 +1,8 @@
 let fs = require('fs');
 let path = require('path');
-let baseFolder = process.argv.slice(2)[0];
-let finalFolder = process.argv.slice(2)[1];
+let baseFolder = process.argv.slice(2)[0] || "./";
+let finalFolder = process.argv.slice(2)[1] || "./sort";
 let deleted = process.argv.slice(2)[2] === 'deleted';
-
-if (!baseFolder) {
-  console.error('Не указана исходная папка!');
-
-  return;
-}
-
-if (!finalFolder) {
-  console.error('Не указана новая папка!');
-
-  return;
-}
 
 if (baseFolder === finalFolder) {
   console.error('Новая папка не должна иметь тот же путь, что и исходная!');
@@ -23,54 +11,98 @@ if (baseFolder === finalFolder) {
 }
 
 const deleteDir = (folder) => {
-  const files = fs.readdirSync(folder);
+  fs.readdir(folder, (err, files) => {
+    if (err) {
+      console.error('Произошла ошибка при чтении папки!');
 
-  files.forEach(file => {
-    let fileName = path.join(folder, file);
-    let state = fs.statSync(fileName);
-
-    if (state.isDirectory()) {
-      deleteDir(fileName);
-    } else {
-      fs.unlinkSync(fileName, (err) => {
-        if (err) throw err;
-      });
+      return;
     }
-  });
 
-  fs.rmdirSync(folder, (err) => {
-    if (err) throw err;
+    files.forEach((file, index) => {
+      let fileName = path.join(folder, file);
+      
+      fs.stat(fileName, (err, state) => {
+        if (err) {
+          console.error('Произошла ошибка при чтении папки!');
+    
+          return;
+        }
+
+        if (state.isDirectory()) {
+          deleteDir(fileName);
+        } else {
+          fs.unlink(fileName, (err) => {
+            if (err) {
+              console.log('Произошла ошибка при удалении файла!');
+            }
+
+            if (index === files.length - 1) {
+              fs.rmdir(folder, (err) => {
+                console.log(err);
+              });
+            }
+          });
+        }
+      });
+    });
   });
 };
 
 const readDir = (folder) => {
-  const files = fs.readdirSync(folder);
+  fs.readdir(folder, (err, files) => {
+    if (err) {
+      console.error('Произошла ошибка при чтении папки!');
 
-  files.forEach(file => {
-    let fileName = path.join(folder, file);
-    let state = fs.statSync(fileName);
-
-    if (state.isDirectory()) {
-      readDir(fileName);
-    } else {
-      let newFolder = path.join(finalFolder, file[0]);
-
-      if (!fs.existsSync(newFolder)) {
-        fs.mkdirSync(newFolder);
-      }
-
-      if (!fs.existsSync(path.join(newFolder, file))) {
-        fs.linkSync(fileName, path.join(newFolder, file));
-      }
+      return;
     }
+
+    files.forEach(file => {
+      let fileName = path.join(folder, file);
+
+      fs.stat(fileName, (err, state) => {
+        if (err) {
+          console.error('Произошла ошибка при чтении папки!');
+    
+          return;
+        }
+
+        if (state.isDirectory()) {
+          readDir(fileName);
+        } else {
+          let newFolder = path.join(finalFolder, file[0]);
+    
+          if (!fs.existsSync(newFolder)) {
+            fs.mkdir(newFolder, (err) => {
+              if (err) {
+                console.error('Произошла ошибка при создании подпапки!');
+              }
+            });
+          }
+    
+          if (!fs.existsSync(path.join(newFolder, file))) {
+            fs.link(fileName, path.join(newFolder, file), (err) => {
+              if (err) {
+                console.error('Произошла ошибка при копировании файла!');
+
+                return;
+              }
+            });
+          }
+        }
+      });
+    });
   });
 };
 
 if (!fs.existsSync(finalFolder)) {
-  fs.mkdirSync(finalFolder);
-}
+  fs.mkdir(finalFolder, (err) => {
+    if (err) {
+      console.log('Произошла ошибка при создании новой папки!')
+    }
 
-readDir(baseFolder);
+    readDir(baseFolder);
+  });
+}
 
 if (deleted) {
   deleteDir(baseFolder);
