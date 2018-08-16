3be7d5d567119be3471a3c8e2fc02f81c47360c3
Feer Aleksandr <feer@retailsuite.io>
2018-08-17 02:05:37 +0300

    async/await

--- sort.js
+++ sort.js
@@ -1,92 +1,75 @@
 let fs = require('fs');
 let path = require('path');
-let baseFolder = process.argv.slice(2)[0] || "./";
-let finalFolder = process.argv.slice(2)[1] || "./sort";
-let deleted = process.argv.slice(2)[2] === 'deleted';
+let baseFolder = "./music";
+let finalFolder = "./sort";
+let deleted = true;
 
-if (baseFolder === finalFolder) {
-  console.error('Новая папка не должна иметь тот же путь, что и исходная!');
-
-  return;
+let getFiles = (folder) => {
+  return new Promise(resolve => {
+    fs.readdir(folder, (err, files) => resolve(files));
+  });
 }
 
-const deleteDir = (folder) => {
-  const files = fs.readdirSync(folder);
+let isDirectory = (file) => {
+  return new Promise(resolve => {
+    fs.stat(file, (err, state) => resolve(state.isDirectory()));
+  });
+}
 
-  files.forEach(file => {
+let createDir = (folder) => {
+  return new Promise(resolve => {
+    fs.mkdir(folder, (err) => resolve());
+  });
+}
+
+let copyFile = (from, to) => {
+  return new Promise(resolve => {
+    fs.link(from, to, (err) => resolve());
+  });
+}
+
+let deleteFile = (file) => {
+  return new Promise(resolve => {
+    fs.unlink(file, (err) => resolve());
+  });
+}
+
+let readDir = async (folder) => {
+  let files = await getFiles(folder);
+
+  files.forEach(async file => {
     let fileName = path.join(folder, file);
-    let state = fs.statSync(fileName);
+    let isDir = await isDirectory(fileName);
 
-    if (state.isDirectory()) {
+    if (isDir) {
+      readDir(fileName);
+    } else {
+      let newFolder = path.join(finalFolder, file[0]);
+      let newFile = path.join(newFolder, file);
+
+      fs.existsSync(newFolder) || await createDir(newFolder);
+      fs.existsSync(newFile) || await copyFile(fileName, newFile);
+    }
+  });
+}
+
+let deleteDir = async (folder) => {
+  let files = await getFiles(folder);
+
+  files.forEach(async file => {
+    let fileName = path.join(folder, file);
+    let isDir = await isDirectory(fileName);
+
+    if (isDir) {
       deleteDir(fileName);
     } else {
-      fs.unlinkSync(fileName, (err) => {
-        if (err) throw err;
-      });
+      await deleteFile(fileName);
     }
-  });
-
-  fs.rmdirSync(folder, (err) => {
-    if (err) throw err;
   });
 };
 
-const readDir = (folder) => {
-  fs.readdir(folder, (err, files) => {
-    if (err) {
-      console.error('Произошла ошибка при чтении папки!');
-
-      return;
-    }
-
-    files.forEach(file => {
-      let fileName = path.join(folder, file);
-
-      fs.stat(fileName, (err, state) => {
-        if (err) {
-          console.error('Произошла ошибка при чтении папки!');
-    
-          return;
-        }
-
-        if (state.isDirectory()) {
-          readDir(fileName);
-        } else {
-          let newFolder = path.join(finalFolder, file[0]);
-    
-          if (!fs.existsSync(newFolder)) {
-            fs.mkdir(newFolder, (err) => {
-              if (err) {
-                console.error('Произошла ошибка при создании подпапки!');
-              }
-            });
-          }
-    
-          if (!fs.existsSync(path.join(newFolder, file))) {
-            fs.link(fileName, path.join(newFolder, file), (err) => {
-              if (err) {
-                console.error('Произошла ошибка при копировании файла!');
-
-                return;
-              }
-            });
-          }
-        }
-      });
-    });
-  });
-};
-
-if (!fs.existsSync(finalFolder)) {
-  fs.mkdir(finalFolder, (err) => {
-    if (err) {
-      console.log('Произошла ошибка при создании новой папки!')
-    }
-
-    readDir(baseFolder);
-  });
-}
-
-if (deleted) {
-  deleteDir(baseFolder);
-}
+(async () => {
+  fs.existsSync(finalFolder) || await createDir(finalFolder);
+  await readDir(baseFolder);
+  deleted || await deleteDir(baseFolder);
+})();
