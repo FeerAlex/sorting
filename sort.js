ac981fd2c4aaf9ceb12f512c257dc5541f780f0d
Feer Aleksandr <feer@retailsuite.io>
2018-08-18 13:25:56 +0300

    fix deleteDir

--- sort.js
+++ sort.js
@@ -1,17 +1,17 @@
 let fs = require('fs');
 let path = require('path');
-let baseFolder = "./music";
-let finalFolder = "./sort";
-let deleted = true;
+let baseFolder = process.argv.slice(2)[0] || './';
+let finalFolder = process.argv.slice(2)[1] || './sort';
+let deleted = process.argv.slice(2)[2] === 'deleted';
 
 let getFiles = (folder) => {
   return new Promise((resolve, reject) => {
     fs.readdir(folder, (err, files) => {
       if (err) reject(err);
-      resolve(files)
+      resolve(files);
     });
   });
-}
+};
 
 let isDirectory = (file) => {
   return new Promise((resolve, reject) => {
@@ -20,7 +20,7 @@ let isDirectory = (file) => {
       resolve(state.isDirectory());
     });
   });
-}
+};
 
 let createDir = (folder) => {
   return new Promise((resolve, reject) => {
@@ -29,7 +29,7 @@ let createDir = (folder) => {
       resolve();
     });
   });
-}
+};
 
 let copyFile = (from, to) => {
   return new Promise((resolve, reject) => {
@@ -38,16 +38,16 @@ let copyFile = (from, to) => {
       resolve();
     });
   });
-}
+};
 
 let deleteFile = (file) => {
   return new Promise((resolve, reject) => {
     fs.unlink(file, (err) => {
       if (err) reject(err);
-      resolve()
+      resolve();
     });
   });
-}
+};
 
 let removeDir = (folder) => {
   return new Promise((resolve, reject) => {
@@ -56,7 +56,7 @@ let removeDir = (folder) => {
       resolve();
     });
   });
-}
+};
 
 let readDir = async (folder) => {
   let files = await getFiles(folder);
@@ -75,13 +75,13 @@ let readDir = async (folder) => {
       fs.existsSync(newFile) || await copyFile(fileName, newFile);
     }
   });
-}
+};
 
 let deleteDir = async (folder) => {
   let files = await getFiles(folder);
 
-  files.forEach(async file => {
-    let fileName = path.join(folder, file);
+  for (let i = 0; i < files.length; ++i) {
+    let fileName = path.join(folder, files[i]);
     let isDir = await isDirectory(fileName);
 
     if (isDir) {
@@ -89,17 +89,22 @@ let deleteDir = async (folder) => {
     } else {
       await deleteFile(fileName);
     }
-  });
+  }
 
   await removeDir(folder);
 };
 
 (async () => {
+  if (baseFolder === finalFolder) {
+    console.error('Новая папка не должна иметь тот же путь, что и исходная!');
+    return;
+  }
+
   try {
     fs.existsSync(finalFolder) || await createDir(finalFolder);
     await readDir(baseFolder);
     !deleted || await deleteDir(baseFolder);
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
+    console.log(err);
   }
 })();
